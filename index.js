795c9f2046c8132234a765042aa757e283f63f76
Surya Prakash Mallah <sfrost488@gmail.com>
2024-03-08 23:31:06 +0530

    completed the Admin routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
@@ -8,76 +8,86 @@ let USERS = [];
 let COURSES = [];
 let courseId = 1;
 
-const adminAuthentication = (req, res, next )=>{
+const adminAuthentication = (req, res, next) => {
   const { username, password } = req.headers;
-  const admin = ADMINS.find(a => a.username === username && a.password === password );
-  if(admin){
+  const admin = ADMINS.find(
+    (a) => a.username === username && a.password === password
+  );
+  if (admin) {
     next();
-  }else {
-    res.status(403).json({ msg : " Admin authentication failed"})
+  } else {
+    res.status(403).json({ msg: " Admin authentication failed" });
   }
 };
 
-
 // Admin routes
-app.post('/admin/signup', (req, res) => {
+app.post("/admin/signup", (req, res) => {
   // logic to sign up admin
   const admin = req.body;
-  const existingAdmin = ADMINS.find(a => admin.username === a.username); 
-  console.log( existingAdmin )
-  if(existingAdmin){
-    res.status(403).json( {msg : 'Admin already registered'})
-  }else {
-    ADMINS.push(admin)
-    res.json({msg : 'Admin created successfully'})
+  const existingAdmin = ADMINS.find((a) => admin.username === a.username);
+  if (existingAdmin) {
+    res.status(403).json({ msg: "Admin already registered" });
+  } else {
+    ADMINS.push(admin);
+    res.json({ msg: "Admin created successfully" });
   }
 });
 
-app.post('/admin/login', adminAuthentication, (req, res) => {
+app.post("/admin/login", adminAuthentication, (req, res) => {
   // logic to log in admin
-  res.json({ msg : "Logged in successfully"})
-  
+  res.json({ msg: "Logged in successfully" });
 });
 
-app.post('/admin/courses', adminAuthentication, (req, res) => {
+app.post("/admin/courses", adminAuthentication, (req, res) => {
   // logic to create a course
   let course = req.body;
   course.Id = courseId++;
-  COURSES.push(course)
-  res.status(200).json({ msg : "Course created successfully"})
+  COURSES.push(course);
+  res.status(200).json({ msg: "Course created successfully" });
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put("/admin/courses/:courseId", adminAuthentication, (req, res) => {
   // logic to edit a course
-  
+  let Id = req.params;
+  let course = req.body;
+
+  let courseIndex = COURSES.findIndex((a) => Id === a.Id);
+  if (courseIndex === -1) {
+    res.status(404).json({ msg: " Course Index not found " });
+  }
+
+  Object.keys(course).forEach((key) => {
+    COURSES[courseId][key] = course.key;
+  });
+  res.status(202).json({ msg: " Course Updated successfully" });
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get("/admin/courses", adminAuthentication, (req, res) => {
   // logic to get all courses
-  
+  res.status(200).json({ course: COURSES });
 });
 
 // User routes
-app.post('/users/signup', (req, res) => {
+app.post("/users/signup", (req, res) => {
   // logic to sign up user
 });
 
-app.post('/users/login', (req, res) => {
+app.post("/users/login", (req, res) => {
   // logic to log in user
 });
 
-app.get('/users/courses', (req, res) => {
+app.get("/users/courses", (req, res) => {
   // logic to list all courses
 });
 
-app.post('/users/courses/:courseId', (req, res) => {
+app.post("/users/courses/:courseId", (req, res) => {
   // logic to purchase a course
 });
 
-app.get('/users/purchasedCourses', (req, res) => {
+app.get("/users/purchasedCourses", (req, res) => {
   // logic to view purchased courses
 });
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
